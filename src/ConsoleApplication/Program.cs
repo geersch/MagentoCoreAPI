accb9c3aa6d8fef7f8e97ce22ca28074d176cf95
Christophe Geers <geersch@gmail.com>
2014-05-31 07:41:52 +0200

    Added source code

---
+++ src/ConsoleApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ConsoleApplication.Version2;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (Mage_Api_Model_Server_V2_HandlerPortTypeClient proxy = new Mage_Api_Model_Server_V2_HandlerPortTypeClient())
+            {
+                string sessionId = proxy.login("your_username", "your_api_key");
+
+                customerCustomerEntity[] customers = proxy.customerCustomerList(sessionId, new filters());
+
+                foreach(customerCustomerEntity customer in customers)
+                {
+                    Console.WriteLine(String.Format("{0}. {1} {2}", customer.customer_id, customer.firstname, customer.lastname));
+                }
+
+                proxy.endSession(sessionId);
+            }
+            Console.WriteLine("Press a key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
